43b61c5358a57fc54624f6fe81be6d5c70b335ca
Sunny Batabyal <batabyalsunny@gmail.com>
2019-04-16 17:38:42 +0530

    Controller and service added for Employee.

---
+++ src/main/java/ml/bootcode/profileapp/services/impl/EmployeeServiceImpl.java
@@ -0,0 +1,81 @@
+/**
+ * 
+ */
+package ml.bootcode.profileapp.services.impl;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import ml.bootcode.profileapp.dto.EmployeeDTO;
+import ml.bootcode.profileapp.services.EmployeeService;
+
+/**
+ * @author sunnyb
+ *
+ */
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see ml.bootcode.profileapp.services.EmployeeService#getEmployees()
+	 */
+	@Override
+	public List<EmployeeDTO> getEmployees() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * ml.bootcode.profileapp.services.EmployeeService#getEmployee(java.lang.Long)
+	 */
+	@Override
+	public EmployeeDTO getEmployee(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see ml.bootcode.profileapp.services.EmployeeService#addEmployee(ml.bootcode.
+	 * profileapp.dto.EmployeeDTO)
+	 */
+	@Override
+	public EmployeeDTO addEmployee(EmployeeDTO employeeDTO) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * ml.bootcode.profileapp.services.EmployeeService#updateEmployee(java.lang.
+	 * Long, ml.bootcode.profileapp.dto.EmployeeDTO)
+	 */
+	@Override
+	public EmployeeDTO updateEmployee(Long id, EmployeeDTO employeeDTO) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * ml.bootcode.profileapp.services.EmployeeService#deleteEmployee(java.lang.
+	 * Long)
+	 */
+	@Override
+	public void deleteEmployee(Long id) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
